681591dd82e87119c6045d6217718f832ac4515c
ymdev <ymdevcompany@gmail.com>
2023-12-31 01:25:07 +0100

    add-benefit

--- src/app/admin/add-benefit-popup/add-benefit-popup.component.ts
+++ src/app/admin/add-benefit-popup/add-benefit-popup.component.ts
@@ -1,22 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-benefit-popup',
   templateUrl: './add-benefit-popup.component.html',
   styleUrls: ['./add-benefit-popup.component.scss']
 })
-export class AddBenefitPopupComponent {
+export class AddBenefitPopupComponent implements OnInit{
   @Output() close = new EventEmitter<void>();
   @Output() submit = new EventEmitter<void>();
-  benefitForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  // @Input() benefitId;
+  benefitForm!: FormGroup;
+  benefit : any;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm(this.benefit);
+  }
+
+  initForm(benefit: any) {
+    let benefitLabel = '';
+    let benefitDescription = '';
+    let benefitRank = 0;
+    let benefitActive = true;
+
     this.benefitForm = this.fb.group({
-      code: [''],
-      label: [''],
-      description: [''],
-      rank: [''],
-      active: [false],
+      'label': new FormControl(benefitLabel, Validators.required),
+      'description': new FormControl(benefitDescription, Validators.required),
+      'rank': new FormControl(benefitRank, Validators.required),
+      'active': new FormControl(benefitActive),
     });
   }
 
